bde0e854bea8a12763a345eb3a774c2e0616680b
beginna <futbeginna.de>
2020-12-30 17:09:25 +0100

    change

--- background.js
+++ background.js
@@ -2,20 +2,15 @@
 
 // listen for any changes to the URL of any tab
 browser.tabs.onUpdated.addListener(function(id, info, tab){
-	console.log(tab.url);
-	//console.log(tab.id);
-
-	// if url matches, display pageAction icon
-	if (tab.url === "https://www.bergfex.at/") {
+	// check url for bergfex; if true show icon; else hide icon
+	if (tab.url.indexOf("bergfex") >-1) {
 		browser.pageAction.show(tab.id);
-		console.log("is TRUE");
-	} else {
-		browser.pageAction.hide(tab.id);
-		console.log("is FALSE");
-	}
-});
-
-// listen for our browerAction to be clicked
+		} else {
+			browser.pageAction.hide(tab.id);
+		}
+	});
+	
+// listen for pageAction/icon to be clicked
 browser.pageAction.onClicked.addListener(function(tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 	browser.tabs.executeScript(tab.id, {
--- inject.js
+++ inject.js
@@ -1,19 +1,40 @@
 // this is the code which will be injected...
 
 (function() {
-	console.log("modifying for mapOnly");
-  
-  
+	console.log("Addon: bergfex OnlyMap: injecting modifications");
+	  
 	document.getElementById('btn-map-show').click();
-	//document.getElementById('infobar').style.visibility = 'hidden';
-	//document.getElementById('footer').style.visibility = 'hidden';
-  
-	document.getElementById('map-layer-skigebiete').click();
-	document.getElementById('map-layer-langlaufen').click();
-	document.getElementById('map-layer-unterkuenfte').click();
-	document.getElementById('map-layer-wintertouren').click();
-  
-	document.querySelector("#siteMapCanvas > div.leaflet-control-container > div.leaflet-top.leaflet-left > div > div:nth-child(1) > div.leaflet-controls-container.leaflet-bar.leaflet-dropdown-controls-container.leaflet-dropdown.leaflet-layers-controls-container > div > div.box-header").click();
+	
+	var mapLayerActivityArr = [
+		"map-layer-skigebiete",
+		"map-layer-skigebiete",
+		"map-layer-langlaufen",
+		"map-layer-unterkuenfte",
+		"map-layer-sommerinfos",
+		"map-layer-wandertouren",
+		"map-layer-alpintouren",
+		"map-layer-wintertouren",
+		"map-layer-biketouren",
+		"map-layer-lauftouren",
+		"map-layer-badeseen"
+	];
+
+	for (var i = 0; i < mapLayerActivityArr.length; i++) {
+		if (document.getElementById(mapLayerActivityArr[i]).className == 'map-layer map-layer-selected') {
+			document.getElementById(mapLayerActivityArr[i]).click()
+		}
+	}
+
+	//document.querySelector("#siteMapCanvas > div.leaflet-control-container > div.leaflet-top.leaflet-left > div > div:nth-child(1) > div.leaflet-controls-container.leaflet-bar.leaflet-dropdown-controls-container.leaflet-dropdown.leaflet-layers-controls-container > div").click();
+	var toogleCC = document.querySelector("#siteMapCanvas > div.leaflet-control-container > div.leaflet-top.leaflet-left > div > div:nth-child(1) > div.leaflet-controls-container.leaflet-bar.leaflet-dropdown-controls-container.leaflet-dropdown.leaflet-layers-controls-container > div");
+	if (toogleCC.classList.contains("collapsed")) {
+		toogleCC.classList.remove("collapsed");
+		toogleCC.classList.add("expanded")
+	} else {
+		toogleCC.classList.remove("expanded");
+		toogleCC.classList.add("collapsed")
+	}
+
 	document.querySelector("#siteMapCanvas > div.leaflet-control-container > div.leaflet-top.leaflet-left > div > div:nth-child(2) > div > div.leaflet-control-fullscreen.leaflet-bar.leaflet-control > a").click();
 	document.querySelector("#siteMap > div.fullscreen-content-container > div.fullscreen-content-toggle").click();
   
