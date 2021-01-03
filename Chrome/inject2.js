ce1d3fb6064b4dc4560ba97e569037085fc20875
beginna <futbeginna.de>
2021-01-03 12:48:26 +0100

    new inject.js for activities

---
+++ Chrome/inject2.js
@@ -0,0 +1,8 @@
+// this is the code which will be injected...
+
+(function() {
+	console.log("Addon: bergfex OnlyMap: injecting modification in activities.");
+	  
+	document.getElementById('mapfullscreen').focus();
+	document.getElementById('mapfullscreen').click();
+});
