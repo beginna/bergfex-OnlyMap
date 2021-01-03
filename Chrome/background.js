c5623b232ace0435e555eaf9dc0f39eae5408740
beginna <futbeginna.de>
2021-01-03 16:23:38 +0100

    clean up

--- Chrome/background.js
+++ Chrome/background.js
@@ -4,8 +4,10 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
 	// check url for bergfex; if true show icon; else hide icon
 	if (tab.url.indexOf("bergfex") >-1) {
-		chrome.pageAction.show(tab.id);
-	} else {chrome.pageAction.hide(tab.id)}
+		chrome.pageAction.show(tab.id)
+	} else {
+		chrome.pageAction.hide(tab.id)
+	}
 });
 
 // listen for pageAction/icon to be clicked
