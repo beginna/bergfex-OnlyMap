25207bee76c64584db829b6ac4898c88e613e9fd
beginna <futbeginna.de>
2021-01-17 15:18:18 +0100

    added debug console.logs

--- Chrome/injectMyTouren.js
+++ Chrome/injectMyTouren.js
@@ -4,11 +4,13 @@
 	console.log("Addon: bergfex OnlyMap: injecting modification for touren map.");
 
 	if (document.querySelector("#mapfullscreen").classList.contains("button-zoomout")) {
+		console.log("debug inject: button zoomout detected. -> full screen to small map");
 		//full screen to small map
 		document.querySelector("#GeosetterWrapper").setAttribute("style", "height: 100%; width: 100%; position: fixed; top: 0px; left: 0px;");
         document.querySelector("#mapfullscreen").setAttribute("class", "map-action map-action-fullscreen button-zoomout");
         document.querySelector("#mapfullscreen").setAttribute("title", "Zoom out")
 		} else {
+			console.log("debug inject: button zoomout NOT detected. -> small map to full screen");
 			//small map to full screen
 			document.querySelector("#GeosetterWrapper").setAttribute("style", "height: 550px; width: 100%; position: relative; top: 0px; left: 0px;");
             document.querySelector("#mapfullscreen").setAttribute("class", "map-action map-action-fullscreen button-fullscreen");
